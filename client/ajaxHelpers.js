ea830cd1c8dcbc0afab001305004e49d23587d9d
SAMURAIM0SES <ericdiliegro@gmail.com>
2022-06-24 10:38:07 -0500

    first commit

---
+++ client/ajaxHelpers.js
@@ -0,0 +1,21 @@
+// Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
+const cohortName = 'COHORT-NAME';
+// Use the APIURL variable for fetch requests
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+
+
+export const fetchAllPlayers = async () => {
+
+};
+
+export const fetchSinglePlayer = async (playerId) => {
+
+};
+
+export const addNewPlayer = async (playerObj) => {
+
+};
+
+export const removePlayer = async (playerId) => {
+
+};
